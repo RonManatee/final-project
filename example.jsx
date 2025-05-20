e56176665846d1d9dc0bee7f98f15f09d589be4e
RonManatee <athompson0103@gmail.com>
2025-05-20 13:35:58 -0400

    Update example.jsx

--- example.jsx
+++ example.jsx
@@ -158,4 +158,165 @@ function myVehicle({ model, registration: { state } }) {
 
 
 //SPREAD OPERATOR
+  //(...) allows us to quickly copy all or part of an existing array or object to another array or object
+  const numbersOne = [1,2,3];
+  const numbersTwo = [4,5,6];
+  const numbersCombined = [...numbersOne, ...numbersTwo];
+  //spread is often used in combination with destructuring
+  const numbers = [1,2,3,4,5,6];
+  const [one, two, ...rest] = numbers;
+  // can use spread with objects also 
+  const myVehicle = {
+    brand: 'Ford',
+    model: 'Escape',
+    color: 'Red'
+  }
+  const updateMyVehicle = {
+    type: 'car',
+    year: 2022.
+    color: 'yellow'
+}
+  const myUpdatedVehicle = {...myVehicle, ...updateMyVehicle}
+
+
+//MODULES
+    //EXPORT, allows to export a function or variable from any file
+    //IN-LINE Export
+    export const name = 'Jesse'
+    export const age = 40
+    //ALL AT ONCE AT THE BOTTOM
+    const name = 'Jesse'
+    const age = 40
+      export {name, age}
+
+    //DEFAULT EXPORTS, can only have ONE default export in a file
+    const message = () => {
+      const name = "jesse";
+      const age = 40;
+      return name + ' is ' + age + ' Years old. ';
+    };
+        export default message;
+
+    //IMPORT, can import modules into a file in two ways; named or default exports
+        //NAMED
+          import { name, age} from "./person.js";
+        //DEFAULT
+          import message from "./message.js";
+
+  //TERNARY OPERATOR
+        //Simplified conditional operator like "if/else"
+        //Syntax: condition ? <expression if true> : <expression if false>
+        function rederApp() {
+          document.getElementById('demo').innerHTML = "Welcome!";
+        }
+          function renderLogin() {
+            document.getElementById('demo').innerHTML = "Please log in";
+          }
+          let authenticated = true; //shows the ternary to be authenticated
+          //TERNARY operator makes the above work correctly
+          if (authenticated) {
+          renderApp();
+        }else{
+          renderLogin();
+        }
+
+        //A SIMPLER METHOD OF TERNARY instead of the "if/else"
+        authenticated ? renderApp() : renderLogin();
+
+  //RENDER HTML
+      //createRoot() is to define the hHTML element where a React component should be displayed.
+      //render() called to define the React component that should be rendered. 
+        import React from  'react';
+        import ReactDOM from 'react-dom/client';
+
+        const container = document.getElementById('root');
+          const root = ReactDOM.createRoot(container);
+          root.render(<p>Hello</p>);
+
+      //ANOTHER EXAMPLE
+         const myelement = (
+            <table>
+              <tr>
+                <th>Name</th>
+              </tr>
+              <tr>
+                <td>John</td>
+                </tr>
+              <tr>
+                <td>Elsa</td>
+                </tr>
+            </table>
+          );
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(myelement); // calls back the const from the beginning
+
+      //ROOT NODE is the HTML element where you want to display the result.
+        //like a container for content managed by React.
+          //does NOT have to be a <div> element and it does NOT have to have the id = 'root'
+      import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('sandy');
+const root = ReactDOM.createRoot(container);
+root.render(<p>Hallo</p>);
+
+/*
+For this example to work on your project,
+you must have a element with
+id="sandy" on your "index.html" page.
+*/
+
+//PROPS are arguments passed into React components. Props are passed to components via HTML attributes.
+//PROPS stand for properties
+  // ADD a 'brand' attribute to the Car element
+    const MyElement = <Car brand='Ford' />;
+          //component receives the arguemtn as a props object;
+          //use the brand attriubute in the component;
+          function Car(props) {
+            return <h2>I am a {props.brand}!</h2>;
+          }
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);  //=== 'I am a Ford!
+
+//PASS DATA
+  //Props are also how you pass data from one component to antoher, as a parameter
+  //Send the 'brand' property from the Garage component to the Car component;
+      function Car(props) {
+        return <h2>I am a {props.brand}!</h2>;
+      }
+        function Garage() {
+          //const carName = "Ford"; this can be used as a variable instead of a string
+          // or if it was an object const carInfo = {name: "ford", model: "mustang" };
+          return (
+            <>
+            <h1>Who lives in my garage?</h1>
+              <Car brand="Ford" /> 
+                                      //if variable, this would change to <Car brand = {carName} />
+                                      //if object <Car brand = {carInfo} />
+            </>
+            );
+        }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<Garage />);
+
+
+//EVENTS are writtin in camelCase syntax. React event handlers are written inside curly braces
+    function Football() {
+  const shoot = () => {
+    alert("Great Shot!");
+  }
+
+  return (
+    <button onClick={shoot}>Take the shot!</button> //returns "Great shot" from the alert
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Football />);
+
+//PASSING ARGUMENTS, to pass an argument to an event handler, use an arrow function
+    <button onClick ={() => shoot("Goal!")}>Take the shot!</button> //returns with Goal upon button click
+
         
